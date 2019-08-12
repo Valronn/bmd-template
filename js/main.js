e3966f5961d7443fbbd58d1763a8b2c2268db5a0
Valronn <charliescenek@gmail.com>
2019-08-12 18:34:44 +0300

    Deleted useless function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
   $("body").bootstrapMaterialDesign();
-  $(".carousel").carousel({
-    interval: 5000,
-    pause: false
-  });
   $(".toggler").click(()=>{
     $(".bar-0").toggleClass("change0");
     $(".bar-1").toggleClass("change1");
